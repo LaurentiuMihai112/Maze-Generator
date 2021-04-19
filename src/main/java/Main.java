711cdc8ec727ed5e5158162df05a052579cb27ce
Mihai Cristian-Laurentiu <mihai.laurentiu24@yahoo.com>
2021-04-19 18:54:14 +0300

    Created project

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -57,22 +57,7 @@ public class Main extends PApplet {
         }
         endShape(CLOSE);
     }
-
-    void star(float x, float y, float radius1, float radius2, int npoints) {
-        float angle = TWO_PI / npoints;
-        float halfAngle = (float) (angle / 2.0);
-        beginShape();
-        for (float a = 0; a < TWO_PI; a += angle) {
-            float sx = x + cos(a) * radius2;
-            float sy = y + sin(a) * radius2;
-            vertex(sx, sy);
-            sx = x + cos(a + halfAngle) * radius1;
-            sy = y + sin(a + halfAngle) * radius1;
-            vertex(sx, sy);
-        }
-        endShape(CLOSE);
-    }
-
+    
     public void hexGrid(int scale, int x, int y) {
         fill(180, 0, 0);
         polygon(x, y, 10, 6);
